366e12577f3b53b15057c2dabd626c0a9b3dfa58
hawebekspres <ha.webekspres@gmail.com>
2026-06-03 12:04:48 +0700

    feat: change type parameter in tiptap rte

--- components/RichTextEditor.tsx
+++ components/RichTextEditor.tsx
@@ -163,7 +163,7 @@ export default function RichTextEditor({
   useEffect(() => {
     if (!editor) return;
     if (editor.getHTML() !== value) {
-      editor.commands.setContent(value || "", false);
+      editor.commands.setContent(value || "", { emitUpdate: false });
     }
   }, [value, editor]);
 
