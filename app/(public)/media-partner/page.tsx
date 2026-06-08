cfd36eaaaef3d784bbfd9fa12d77ff19d23b62de
hawebekspres <ha.webekspres@gmail.com>
2026-06-08 13:53:05 +0700

    feat: enable dynamic rendering for multiple public pages

--- app/(public)/media-partner/page.tsx
+++ app/(public)/media-partner/page.tsx
@@ -1,5 +1,7 @@
 import { createInfoPage } from '@/lib/create-info-page';
 
+export const dynamic = 'force-dynamic';
+
 const { generateMetadata, default: MediaPartnerPage } = createInfoPage('media-partner');
 
 export { generateMetadata };
