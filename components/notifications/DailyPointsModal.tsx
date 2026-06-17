1af4c6204f6f4560ff2438eabc360adc54e8dde8
hawebekspres <ha.webekspres@gmail.com>
2026-06-17 15:58:07 +0700

    fix:tailwind syntax warn

--- components/notifications/DailyPointsModal.tsx
+++ components/notifications/DailyPointsModal.tsx
@@ -18,9 +18,9 @@ export default function DailyPointsModal({
   return (
     <DialogPrimitive.Root open={open} onOpenChange={(next) => !next && onDismiss()}>
       <DialogPrimitive.Portal>
-        <DialogPrimitive.Overlay className="fixed inset-0 z-[60] bg-black/50 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
+        <DialogPrimitive.Overlay className="fixed inset-0 z-60 bg-black/50 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
         <DialogPrimitive.Content
-          className="fixed left-1/2 top-1/2 z-[60] w-[min(100vw-2rem,24rem)] -translate-x-1/2 -translate-y-1/2 border border-jepang-border bg-white p-6 shadow-jepang-lg"
+          className="fixed left-1/2 top-1/2 z-60 w-[min(100vw-2rem,24rem)] -translate-x-1/2 -translate-y-1/2 border border-jepang-border bg-white p-6 shadow-jepang-lg"
           data-testid="daily-points-modal"
         >
           <div className="flex items-start justify-between gap-3">
