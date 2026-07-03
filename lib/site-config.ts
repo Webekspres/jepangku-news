65a878ccb4f6a3770297ee2f4f1afb0dbba08c3e
hawebekspres <ha.webekspres@gmail.com>
2026-07-03 16:44:34 +0700

    feat: change description page and change og image

--- lib/site-config.ts
+++ lib/site-config.ts
@@ -2,7 +2,7 @@ export const SITE_BRAND_NAME = "JepangKu";
 export const SITE_DEFAULT_TITLE = `${SITE_BRAND_NAME} | Pusat Ekosistem Jepang`;
 export const SITE_TITLE_TEMPLATE = `%s | ${SITE_BRAND_NAME}`;
 export const SITE_DEFAULT_DESCRIPTION =
-  "Pusat ekosistem Jepang untuk pembaca Indonesia. Baca artikel, tonton video, belajar bahasa, ikut kuis & polling, raih poin!";
+  "Jepang Versi kamu. Baca artikel, tonton video, belajar bahasa, ikut kuis & polling, raih poin!";
 
 export type {
   SocialLink,
